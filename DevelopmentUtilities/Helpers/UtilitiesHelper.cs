3ab225db5807039d4bf4a36cb13f6b1369e013e6
Alex Marin Liebana <alex.marin@asolcon.com>
2022-09-03 16:41:29 +0200

    Moved functions to helper

---
+++ DevelopmentUtilities/Helpers/UtilitiesHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace DevelopmentUtilities.Helpers
+{
+    public static class UtilitiesHelper
+    {
+        public static bool IsNumber(string FieldType)
+        {
+            switch (FieldType)
+            {
+                case "int": return true;
+                case "float": return true;
+                case "double": return true;
+                case "decimal": return true;
+                default: return false;
+            }
+        }
+    }
+}
